c85b0fdd2b30a63075fc27daa0c219f98c717cc8
faisalsahak <sahak.faisal@gmail.com>
2018-12-26 22:57:18 -0800

    added the matrices that correspond to each shape in a switch statement

--- clientComponent/components/Piece.js
+++ clientComponent/components/Piece.js
@@ -17,4 +17,52 @@ class Piece {
     return 'TLJSZOI'[Math.random() * 7 | 0]
   }
 
+    createPiece(type) {
+    switch (type) {
+      case 'T':
+        return ([
+              [0,1,0],
+              [1,1,1],
+              [0,0,0],
+            ])
+      case 'L':
+        return ([
+              [0,0,2],
+              [2,2,2],
+              [0,0,0],
+            ])
+      case 'O':
+        return ([
+              [3,3],
+              [3,3],
+            ])
+      case 'J':
+        return ([
+              [4,0,0],
+              [4,4,4],
+              [0,0,0],
+            ])
+      case 'I':
+        return ([
+              [0,5,0,0],
+              [0,5,0,0],
+              [0,5,0,0],
+              [0,5,0,0],
+            ])
+      case 'S':
+        return ([
+              [0,6,6],
+              [6,6,0],
+              [0,0,0],
+            ])
+      case 'Z':
+        return ([
+              [7,7,0],
+              [0,7,7],
+              [0,0,0],
+            ])
+    }
+  }
+
+
 }
